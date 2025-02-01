dda588034d646681704bfd18de934c469ba1ef16
mhy908 <mhy908@gmail.com>
2025-02-01 19:53:56 +0900

    transfer

--- demo/src/main/kotlin/com/example/demo/MainController.kt
+++ demo/src/main/kotlin/com/example/demo/MainController.kt
@@ -156,7 +156,7 @@ class MainController {
         model.addAttribute("fromUser", fromUser)
         model.addAttribute("message", message)
         model.addAttribute("success", success==2)
-        model.addAttribute("balance", fromUser.balance)
+        model.addAttribute("balance", userRepository.findUser(id)!!.balance)
 
         return "transfer"  // 메시지를 포함한 템플릿 렌더링
     }
